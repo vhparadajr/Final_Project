dd77916b2d0b62e3494db5a44b3199a822f4c22f
vhparadajr <vhparadajr@gmail.com>
2018-07-23 23:36:04 -0400

    Working API

--- js/main.js
+++ js/main.js
@@ -4,23 +4,20 @@ $(document).ready(function(){
   $("#stop").on("submit", function(e){
     e.preventDefault();
     // var stop = "?action=restart";
-<<<<<<< HEAD
-    var apiKey = "0d6af9fca12f4c3188c836c79e39403c";
+    var apiKey = "?key=0d6af9fca12f4c3188c836c79e39403c";
     var apiURL = "http://24.34.240.165:17790/api/summary";
-=======
-    // var apiKey = "464729fb";
-    var apiURL = "http://desktop-0dq00d9:17790/api/summary";
->>>>>>> 949df33a79085309257aa43d97d7821ab0574d16
 
 
     $.ajax({
       type: "GET",
-        url: apiURL,
+        url: apiURL + apiKey,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
       // Work with the response
       success: function(res) {
         console.log(res)
+        console.log(res.algorithmList[0].name)
+        $('#spanAlgo').text(res.algorithmList[0].name)
       },
       error: function(r){
         console.log(r)
